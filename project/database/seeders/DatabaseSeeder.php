0e37fed3ec31801418a081ca8627700f9ddcaa87
Mostafazzz115 <160529840+Mostafazzz115@users.noreply.github.com>
2025-01-22 22:41:27 +0200

    Laravel Breeze Auth

--- project/database/seeders/DatabaseSeeder.php
+++ project/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+       User::create([
+        'name' => 'mostafa',
+        'email' => 'mostafazzz115@gmail.com',
+        'password' => Hash::make('password'),
+        'phone_number' => '01116864977',
+       ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       DB::table('users')->insert([
+        'name' => 'mohamed',
+        'email' => 'mohamedzzz115@gmail.com',
+        'password' => Hash::make('password'),
+        'phone_number' => '01116864978',
+       ]);
     }
 }
--- project/resources/views/layouts/admin/sidebar.blade.php
+++ project/resources/views/layouts/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="{{ asset('assets/backend') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
 
     <!-- Sidebar -->
@@ -13,7 +13,12 @@
           <img src="{{ asset('assets/backend') }}/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }} </a>
+          <br>
+          <form action="{{ route('logout') }}" method="post">
+            @csrf
+            <button type="submit" class="btn btn-small btn-outline-primary">Logout</button>
+          </form>
         </div>
       </div>
 
